7a25b85996dca743ac1e98f72a65a4eedade09a8
Ryan Hefner <hi@ryanhefner.com>
2017-03-14 15:54:36 -0400

    Add basic auth support if configured.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
+  if ENV['HTTP_USERNAME'] and ENV['HTTP_PASSWORD']
+    http_basic_authenticate_with name: ENV['HTTP_USERNAME'], password: ENV['HTTP_PASSWORD']
+  end
+
   def index
     @projects = Project.all
   end
